256883e036d19d48548e5a79deffaba0ebce98aa
Paul Straberger <p.straberger@gmail.com>
2019-11-05 21:53:33 +0100

    fixed even more shit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from genes import Genome
 from creatures import Creature
 
 import pygame
+import pymunk.pygame_util
 import pymunk
 import pygame.locals
 
@@ -18,10 +19,24 @@ karl_genome = Genome()
 karl_genome.append_gene("shape", [0.2, 0.4, 0.8, 1, 1, 0.7, 0.5, 0.2])
 karl_genome.append_gene("width", 100)
 karl_genome.append_gene("length", 400)
-karl_genome.append_gene("thrusters", [[]])
+# implement mirroring for thruster gene
+karl_genome.append_gene("thrusters", [[1,0], [2,0], [4,0], [5,0]])
 
 # creating creature with given genome
 karl = Creature(karl_genome)
+karl.build_phenotype()
+space.add(karl.body, karl.shape)
 
-karl_points = karl.build_phenotype()
-print(karl_points)
+
+# Main loop
+game_running = True
+while game_running:
+   ev = pygame.event.poll()
+   if ev.type == pygame.QUIT:
+       pygame.quit()
+
+   screen.fill((255, 255, 255))
+   draw_options = pymunk.pygame_util.DrawOptions(screen)
+   space.debug_draw(draw_options)
+   space.step(0.02)
+   pygame.display.flip()
