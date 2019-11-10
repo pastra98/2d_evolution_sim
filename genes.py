4e1d5a521f5782f0c92aed0bb7879d1899afc37b
Paul Straberger <p.straberger@gmail.com>
2019-11-10 22:04:56 +0100

    added gene class, changed append func to allow multiple genes at once, switched extract_dna to transcribe genome to work with new Transcriptor class

--- genes.py
+++ genes.py
@@ -1,26 +1,50 @@
+from transcriptors import Transcriptor
+
+# Holds DNA and performs all relevant actions
 class Genome:
-    """class that holds the dna with all it's genes.
+    """class that holds the dna with all it's genes. Also contains
+    a Transcriptor object that is required for expressing gene
+    functions.
     """
     def __init__(self):
-        """ innitializes and creates dna
+        """initializes dna and transcriptor used for reading and
+        expressing genes.
         """
         self.dna = []
 
-
-    def append_gene(self, g_type, g_data):
-        """creates a gene, appends a gene to the dna, and returns it.
+    def append_genes(self, genes):
+        """appends multiple genes to the dna.
         """
-        gene = [g_type, g_data]
-        self.dna.append(gene)
-        return gene
+        for gene in genes:
+            self.dna.append(gene)
 
-
-    def extract_dna(self, g_type):
-        """returns the data of first gene of described type in dna.
-        Could later be changed to return a list of multiple genes
-        describing the same thing.
+    def transcribe_genome(self):
+        """Reads a gene, determines if it should be expressed and
+        assigns it to a list, based on its type. This list represents
+        the data of all genes of a given type. All gene types are then
+        added to a dictionary. The transcriptor is then fed this dict.
         """
+        self.to_transcribe = {}
         for gene in self.dna:
-            if gene[0] == g_type:
-                return gene[1]
+            if gene.is_expressed:
+                if gene.type in self.to_transcribe:
+                    self.to_transcribe[gene.type].append(gene)
+                else:
+                    self.to_transcribe[gene.type] = [gene]
+
+        self.transcriptor = Transcriptor(self.to_transcribe)
+
+
+# Describes a single gene
+class Gene:
+    """class that describes a single gene
+    """
+    def __init__(self, g_type, g_data):
+        """initializes a gene of given type with given data
+        """
+        self.type = g_type
+        self.data = g_data
+        self.is_expressed = True
+
+
 
