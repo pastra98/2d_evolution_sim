2a6c9bc6d78f98ab77aa0c6058c2e1ac6a44cbdc
Paul Straberger <p.straberger@gmail.com>
2019-11-12 14:02:17 +0100

    adjusted file for new thruster objects, will re-implement movement control as soon as ThrusterSystem class is up and running

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ karl_genome = Genome()
 shape_gene = Gene("shape", [0.4, 0.8, 0.7, 0.3])
 width_gene = Gene("width", 50)
 length_gene = Gene("length", 200)
-thr1_gene = Gene("thruster", [0, "lr", 0, 90])
-thr2_gene = Gene("thruster", [3, "lr", 90, 180])
+thr1_gene = Gene("thruster", [0, True, 0, 90])
+thr2_gene = Gene("thruster", [3, True, 90, 180])
 karl_genepool = [shape_gene, width_gene, length_gene, thr1_gene, thr2_gene]
 # adding genes to karl
 karl_genome.append_genes(karl_genepool)
@@ -30,6 +30,9 @@ karl_genome.append_genes(karl_genepool)
 # creating creature with given genome
 karl = Creature(karl_genome)
 karl.birth()
+for thruster in karl.data_dict["thruster_l"]:
+    print(thruster.point, thruster.start_a, thruster.end_a)
+
 space.add(karl.body, karl.shape)
 
 # collection of turning instructions
@@ -53,19 +56,19 @@ while game_running:
     if ev.type == pygame.QUIT:
         pygame.quit()
     # move based on key input
-    elif ev.type == pygame.KEYDOWN:
-        if ev.key == pygame.locals.K_UP:
-            karl.update_directions(forward)
-            karl.apply_thrust([2, 3])
-        elif ev.key == pygame.locals.K_DOWN:
-            karl.update_directions(back)
-            karl.apply_thrust([0, 1])
-        elif ev.key == pygame.locals.K_LEFT:
-            karl.update_directions(left)
-            karl.apply_thrust([0, 3])
-        elif ev.key == pygame.locals.K_RIGHT:
-            karl.update_directions(right)
-            karl.apply_thrust([1, 2])
+    # elif ev.type == pygame.KEYDOWN:
+    #     if ev.key == pygame.locals.K_UP:
+    #         karl.update_directions(forward)
+    #         karl.apply_thrust([2, 3])
+    #     elif ev.key == pygame.locals.K_DOWN:
+    #         karl.update_directions(back)
+    #         karl.apply_thrust([0, 1])
+    #     elif ev.key == pygame.locals.K_LEFT:
+    #         karl.update_directions(left)
+    #         karl.apply_thrust([0, 3])
+    #     elif ev.key == pygame.locals.K_RIGHT:
+    #         karl.update_directions(right)
+    #         karl.apply_thrust([1, 2])
 
     screen.fill((255, 255, 255))
     draw_options = pymunk.pygame_util.DrawOptions(screen)
