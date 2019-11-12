516acc89082f88c40cdd236378c3e9ec93437226
Paul Straberger <p.straberger@gmail.com>
2019-11-12 14:00:30 +0100

    thrusters are now objects with their own methods. Will need to implement thrusterSystem class to track all thrusters and update/fire them in batches

--- creatures.py
+++ creatures.py
@@ -36,33 +36,4 @@ class Creature:
         # if self.applyThr:
         #     self.apply_thrust()
 
-    def update_directions(self, directions):
-        """Passes multiple direction instructions to self.update_directions
-        instr[0] : Nr. of thruster, instr[1] : percent of firing radius
-        """
-        for instr in directions:
-            thr_index = instr[0]
-            firing_rad = instr[1]
-            start_a = self.thruster_l[thr_index][2] # indexed in thr_l
-            end_a = self.thruster_l[thr_index][3] # indexed in thr_l
-            self.update_direction(thr_index, firing_rad, start_a, end_a)
-
-    def update_direction(self, thr_index, firing_rad_percent, start_a, end_a):
-        """changes thrust vector (thruster[4]) of specified thruster.
-        Method: calculates angle within firing radius. Angle is determined by:
-        If fire_rad = 0. -> angle = start_a, fire_rad = 1. -> angle = end_a
-        """
-        new_angle = start_a + firing_rad_percent * (end_a - start_a)
-        # vec pointing 9 o'clock is rotated clockwise
-        vec = pygame.math.Vector2(-1, 0).rotate(-new_angle)
-        self.thruster_l[thr_index][4] = vec # update vec component
-
-    def apply_thrust(self, applyTo):
-        """applies thrust to every thruster in thruster list.
-        applyTo : for now just list with int of every thruster to fire.
-        """
-        for thr_number in applyTo:
-            thr = self.thruster_l[thr_number]
-            self.body.apply_impulse_at_local_point(-thr[4], thr[0])
-
 
