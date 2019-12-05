729c79b81110b2dc99fcd8f8bb35971bf314f07d
Paul Straberger <p.straberger@gmail.com>
2019-12-05 13:01:12 +0100

    re-implemented shitty movement system

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ karl_genome = Genome()
 shape_gene = Gene("shape", [0.4, 0.8, 0.7, 0.3])
 width_gene = Gene("width", 50)
 length_gene = Gene("length", 200)
-thr1_gene = Gene("thruster", [0, True, 0, 90])
+thr1_gene = Gene("thruster", [1, True, 0, 90])
 thr2_gene = Gene("thruster", [3, True, 90, 180])
 karl_genepool = [shape_gene, width_gene, length_gene, thr1_gene, thr2_gene]
 # adding genes to karl
@@ -30,16 +30,7 @@ karl_genome.append_genes(karl_genepool)
 # creating creature with given genome
 karl = Creature(karl_genome)
 karl.birth()
-for thruster in karl.data_dict["thruster_l"]:
-    print(thruster.point, thruster.start_a, thruster.end_a)
-
 space.add(karl.body, karl.shape)
-
-# collection of turning instructions
-forward  = [[2, 1], [3, 1]]
-back = [[0, 0], [1, 0]]
-left = [[0, 1], [3, 0]]
-right = [[1, 1], [2, 0]]
 
 # temporary obstacle setup
 line_moment = pymunk.moment_for_segment(0, (0, 0), (500, 300), 10)
@@ -56,19 +47,19 @@ while game_running:
     if ev.type == pygame.QUIT:
         pygame.quit()
     # move based on key input
-    # elif ev.type == pygame.KEYDOWN:
-    #     if ev.key == pygame.locals.K_UP:
-    #         karl.update_directions(forward)
-    #         karl.apply_thrust([2, 3])
-    #     elif ev.key == pygame.locals.K_DOWN:
-    #         karl.update_directions(back)
-    #         karl.apply_thrust([0, 1])
-    #     elif ev.key == pygame.locals.K_LEFT:
-    #         karl.update_directions(left)
-    #         karl.apply_thrust([0, 3])
-    #     elif ev.key == pygame.locals.K_RIGHT:
-    #         karl.update_directions(right)
-    #         karl.apply_thrust([1, 2])
+    elif ev.type == pygame.KEYDOWN:
+        if ev.key == pygame.locals.K_w:
+            karl.thruster_sys.move("forward")
+        elif ev.key == pygame.locals.K_s:
+            karl.thruster_sys.move("backward")
+        elif ev.key == pygame.locals.K_a:
+            karl.thruster_sys.move("left")
+        elif ev.key == pygame.locals.K_d:
+            karl.thruster_sys.move("right")
+        elif ev.key == pygame.locals.K_q:
+            karl.thruster_sys.turn("left")
+        elif ev.key == pygame.locals.K_e:
+            karl.thruster_sys.turn("right")
 
     screen.fill((255, 255, 255))
     draw_options = pymunk.pygame_util.DrawOptions(screen)
