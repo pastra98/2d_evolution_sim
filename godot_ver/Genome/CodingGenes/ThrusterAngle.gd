30ab23d1ff7ad73b46abb0407c28732093817e51
Paul Straberger <p.straberger@gmail.com>
2020-01-07 13:01:40 +0100

    changed angles to radians

--- godot_ver/Genome/CodingGenes/ThrusterAngle.gd
+++ godot_ver/Genome/CodingGenes/ThrusterAngle.gd
@@ -7,9 +7,9 @@ func _init (gradient, is_dominant: bool, g_name := "ThrusterAngle").(is_dominant
 
 
 func get_angle(decimal: int):
-	"""Returns an angle ranging from 0 to 180 depending on the position
+	"""Returns an angle in radians depending on the position
 	of the calling segment. This position is expressed as an int ranging from
 	0 to 11, and is the parameter for decimal.
 	"""
 	var gradient = expression_gradient.get_gradient(decimal)
-	return 180 * gradient
+	return PI * gradient # PI is 180 degrees (I thought it should be 3PI)
