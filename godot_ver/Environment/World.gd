5f2a4e6f6f30cee7a9d65c7188cf399aabdf4550
Paul Straberger <p.straberger@gmail.com>
2020-01-07 13:00:24 +0100

    Made food placement work with moving viewports.

--- godot_ver/Environment/World.gd
+++ godot_ver/Environment/World.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 var Food = preload("Food.tscn")
 
@@ -8,5 +8,6 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.is_pressed():
 			var new_food = Food.instance()
-			new_food.set_global_position(event.position)
+
+			new_food.set_global_position(get_global_mouse_position())
 			add_child(new_food)
