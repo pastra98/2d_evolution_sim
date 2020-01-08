02c4f28ea0673f530d1459fca6b15fe5d06e4330
Paul Straberger <p.straberger@gmail.com>
2020-01-08 15:59:20 +0100

    added random point to target

--- godot_ver/Translator/Parts/Brain.gd
+++ godot_ver/Translator/Parts/Brain.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var closest_food
+var rng = RandomNumberGenerator.new()
+var next_random_choice = 1
+
 onready var sensor = get_node("../Sensor") # make this defined procedurally
 onready var thruster_system = get_node("../ThrusterSystem")
 
@@ -23,3 +26,12 @@ func find_closest_food():
 				closest_food = area
 	if typeof(closest_food) != TYPE_NIL:
 		return closest_food.global_position
+	# if no food is found pick random target and don't change until end of counter	
+	next_random_choice -= 1
+	if next_random_choice < 0:
+		next_random_choice = 20
+		rng.randomize()
+		var rand_x = rng.randi_range(-800, 800)
+		var rand_y = rng.randi_range(-800, 800)
+		print("random location")
+		return global_position + Vector2(rand_x, rand_y)
