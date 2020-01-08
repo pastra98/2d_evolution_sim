685bf994d47c7ea49138b9d0af14773efef82ee8
Paul Straberger <p.straberger@gmail.com>
2020-01-08 16:02:02 +0100

    .

--- godot_ver/Translator/Parts/ThrusterSystem.gd
+++ godot_ver/Translator/Parts/ThrusterSystem.gd
@@ -1,47 +1,35 @@
 extends Node2D
 
 var test: Vector2
-var thrusters: Array # thrusters are already child, maybe change this?
 var target: Vector2
 var time = 0
  
 var desired_direction: Vector2
-var target_vector = target - position
 
 func update_movement():
 	var desired_angle = desired_ocs_impulse_angle()
 	movement_decision(desired_angle)
-
-	desired_direction = Vector2(80,0).rotated(desired_angle)
-	target_vector = target - global_position
-	print(target, position)
-	update()
+	
+	desired_direction = (target - global_position).rotated(-get_parent().rotation)
+	update() # update the drawn vectors
 
 
 func _draw():
-	draw_line(Vector2(0,0), desired_direction, Color.white, 2)
-	draw_line(Vector2(0,0), target_vector, Color.yellow, 2)
-	for t in thrusters:
-		draw_line(t.attach_point, t.start_vec_draw, Color.blue, 5)
-		draw_line(t.attach_point, t.end_vec_draw, Color.green, 5)
-		pass
+	draw_line(Vector2(0,0), desired_direction, Color.yellow, 2)
 
 
 func desired_ocs_impulse_angle():
 	"""I fucking hate angles so much. Worst code ever.
 	"""
-	var desired_impulse = target - global_position 
-	var creature_rotation = get_node("..").rotation
-	desired_impulse = desired_impulse.rotated(-creature_rotation)
-	# desired_impulse = desired_impulse * -1
-	return desired_impulse.angle_to(Vector2(-1,0))
+	var desired_impulse = (target - global_position) * -1
+	desired_impulse = desired_impulse.angle_to(Vector2(1,0))
+	desired_impulse += get_parent().rotation
+	return desired_impulse
 
 
 func movement_decision(desired_angle):
-	var some_list = []
-
-	for thruster in thrusters:
+	for thruster in get_children():
 		if thruster.get_movement_efficiency(desired_angle) > 0.9:
-			var imp_pos = thruster.attach_point
+			var imp_pos = thruster.position
 			var imp_vec = Vector2(5,0).rotated(desired_angle) * -1
 			get_parent().apply_impulse(imp_pos, imp_vec)
