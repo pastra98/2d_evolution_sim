502b3f72766f6442ded0684f272b24dfe2a62eff
Paul Straberger <p.straberger@gmail.com>
2020-01-08 16:01:00 +0100

    thruster will change, testing in new branch

--- godot_ver/Translator/Parts/Thruster.gd
+++ godot_ver/Translator/Parts/Thruster.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
-var attach_point: Vector2
 var angle: float
 var min_angle: float
 var max_angle: float
+
 var arc: float
 var strength: float
 
-var start_vec_draw: Vector2
-var end_vec_draw: Vector2
+# debugging vars, fix this shit
+var draw_angle: float
+var update_draw = false
 
 func _init(attach_point, angle, arc, strength):
-	self.attach_point = attach_point 
+	self.position = attach_point 
 	self.strength = strength
 	self.angle = angle
 	self.arc = arc
@@ -25,15 +26,17 @@ func _init(attach_point, angle, arc, strength):
 		arc_limits.append(a)
 	self.min_angle = arc_limits[0]
 	self.max_angle = arc_limits[1]
-
-	self.start_vec_draw = attach_point + Vector2(20,0).rotated(angle - arc/2)
-	self.end_vec_draw = attach_point + Vector2(20,0).rotated(angle + arc/2)
+	_draw()
  
  
 func get_movement_efficiency(target_angle):
+	update_draw = false
 	var projection = 0
 	# make this func also return which angle to use!!!
 	if target_angle > (angle - arc/2) and target_angle < (angle + arc/2):
+		update_draw = true
+		draw_angle = target_angle
+		update()
 		return 1 # Max efficiency
 	for p_angle in [min_angle, max_angle]:
 		if (target_angle * p_angle) < 0: # signs don't match
@@ -43,3 +46,10 @@ func get_movement_efficiency(target_angle):
 		elif cos(target_angle - p_angle) > projection:
 			projection = cos(target_angle - p_angle)
 	return projection 
+
+
+func _draw():
+	draw_line(position, position + Vector2(20,0).rotated(angle - arc/2), Color.white, 3)
+	draw_line(position, position + Vector2(20,0).rotated(angle + arc/2), Color.white, 3)
+	if update_draw:
+		draw_line(position, Vector2(60,0).rotated(draw_angle), Color.yellow, 5)
